d962664f540125bce6c7311ff1337c28952f850b
Tim Tu <mitim77@googlemail.com>
2023-11-15 19:05:03 +0000

    milestone_2.py file created and add get_random_word function and get_single_letter function. Start to ducument project experiences in READNE.md file

---
+++ milestone_2.py
@@ -0,0 +1,21 @@
+import random
+word_list = ["apple" , "grape" , "watermelon" , "orange" , "banana"]
+# print(word_list)
+
+def get_random_word() -> None :
+    word = random.choice(word_list)
+    print(word)
+    
+def get_single_letter():
+    decider = True
+    while decider == True:
+        guess = input("Please enter a single letter: ")
+        #print(f'You have entered " {guess} ".')
+        if len(guess) == 1 and guess.isalpha() == True:
+            print("Good guess!")
+        else:
+            print("Oops! That is not a valid input.")
+        return guess
+    
+get_random_word()
+get_single_letter()
