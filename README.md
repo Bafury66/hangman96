680a822194c82322413db4c84b081b82562450f4
Tim Tu <mitim77@googlemail.com>
2023-11-16 22:03:14 +0000

    update milestone_3.py to complete letter check function, update README.md with more details.

--- README.md
+++ README.md
@@ -6,11 +6,24 @@ This is an implementation of the Hangman game, where the computer thinks of a wo
 # Table of Contents, if the README file is long
 
 # A description of the project: what it does, the aim of the project, and what you learned
-
+- The project is aim to build a code for classic game Hangman. Project started with some codes which would select ramdom word from a list of words.
+- Followed by more codes to ask user to give an input as single letter.
+- Code checks if the given input is valid, if it does, proceed to next. Checks include 
+    - if given letter is single letter?
+    - if given letter is alphabet?
+    - if given letter exisit within randomly choosen word?
 # Installation instructions
+
+N/A
 
 # Usage instructions
 
+User will have to run Python file directly - subject to change
+
 # File structure of the project
 
+To be updated later
+
 # License information
+
+To be decided later when codes are completed
--- milestone_3.py
+++ milestone_3.py
@@ -3,35 +3,38 @@ Python code to continuously ask the user for a letter and validate it.
 
 
 '''
-import random
+import random , os
 word_list = ["apple" , "grape" , "watermelon" , "orange" , "banana"]
 print(word_list)
 
 word = random.choice(word_list)
 print(word)
-    
-condition = True
-while condition == True:
-    guess = input("Please guess and enter a single letter and hit Enter: ")
-    if len(guess) == 1 and guess.isalpha() == True:
-        #print("Oh yeah, that's what I need.")
-        print(f'You have entered "{guess}".')
-        break
+
+def check_guess(guess = str):
+    guess = guess.lower()
+    guess_in_word = False
+    for character in word:
+        if guess == character:
+            guess_in_word = True
+            break
+
+    if guess_in_word == True:
+        print(f'Good guess! "{guess}" is in the word.')
     else:
-        print(f'Invalid letter "{guess}". Please, enter a single alphabetical character.')
+        print(f'Sorry, "{guess}" is not in the word. Try again.')
 
-guess_in_word = False
-for character in word:
-    if guess == character:
-        guess_in_word = True
-        break
-
-
-if guess_in_word == True:
-    print(f'Good guess! "{guess}" is in the word.')
-else:
-    print(f'Sorry, "{guess}" is not in the word. Try again.')
-
-        
-
+def ask_for_input():
+    condition = True
+    while condition == True:
+        guess = input("Please guess and enter a single letter and hit Enter: ")
+        if len(guess) == 1 and guess.isalpha() == True:
+            #print("Oh yeah, that's what I need.")
+            print(f'You have entered "{guess}".')
+            break
+        else:
+            print(f'Invalid letter "{guess}". Please, enter a single alphabetical character.')
+            
+    check_guess(guess)
+    
+ask_for_input()
 
